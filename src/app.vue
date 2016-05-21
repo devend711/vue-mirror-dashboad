7182dd832da677d57fd3b8c4c55194998dce0291
devend711 <devend711@gmail.com>
2016-05-21 14:58:18 -0400

    init

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+import TimeManager from './objects/time_manager'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!',
+      timeManager: new TimeManager()
+    }
+  },
+  ready: function () {
+    this.timeManager.startClock()
+  }
+}
+</script>
---
+++ src/objects/time_manager.js
@@ -0,0 +1,43 @@
+function TimeManager() {
+  this.alerts = {
+    second: 1,
+    minute: 1,
+    hour: 1,
+    day: 1
+  }
+  this.currentTime = null
+}
+
+TimeManager.prototype.startClock = function () {
+  var that = this;
+  setTimeout(function () {
+    that.alertSecond()
+    that.startClock()
+  }, 1000)
+}
+
+TimeManager.prototype.alertSecond = function () {
+  this.alerts.second += 1
+  if(this.alerts.second >= 60) {
+    this.alerts.second = 0
+    this.alerts.minute += 1
+  }
+}
+
+TimeManager.prototype.alertMinute = function () {
+  this.alerts.minute += 1
+  if(this.alerts.minute >= 60) {
+    this.alerts.minute = 0
+    this.alerts.hour += 1
+  }
+}
+
+TimeManager.prototype.alertHour = function () {
+  this.alerts.hour += 1
+  if(this.alerts.hour >= 24) {
+    this.alerts.hour = 0
+    this.alerts.day *= -1
+  }
+}
+
+export default TimeManager
